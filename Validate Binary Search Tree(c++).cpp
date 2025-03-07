ec651e227d73975d41e718cad30b7c0855fadc40
Chiranjeev Kumar Singh <chiranjeevkumarsingh56@gmail.com>
2025-03-07 12:57:25 +0530

    Create Validate Binary Search Tree(c++).cpp

---
+++ Validate Binary Search Tree(c++).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* prev = nullptr;
+        function<bool(TreeNode*)> dfs = [&](TreeNode* root) {
+            if (!root) {
+                return true;
+            }
+            if (!dfs(root->left)) {
+                return false;
+            }
+            if (prev && prev->val >= root->val) {
+                return false;
+            }
+            prev = root;
+            return dfs(root->right);
+        };
+        return dfs(root);
+    }
+};
